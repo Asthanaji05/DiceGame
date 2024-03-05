88c48eb2470c8f421f278f6bbe30506f1be3935c
SHASHANK ASTHANA <118747421+Asthanaji05@users.noreply.github.com>
2024-03-05 21:56:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 var random1=Math.floor(Math.random()*6)+1;
 
-// Generate a random number between 1 and 6
-var random1 = Math.floor(Math.random() * 6) + 1;
+var randomimage= "dice"+random1+".png";
 
-// Construct the filename using the random number
-var randomimage = "dice" + random1 + ".png";
+var source="./image/"+randomimage;
 
-// Set the source attribute of the img element with class "img1" to the randomly selected image
-document.querySelector(".img1").setAttribute("src", randomimage);
+document.querySelector(".img1").setAttribute("src",source);
 
 
 var random2=Math.floor(Math.random()*6)+1;
 
 var randomimage2= "dice"+random2+".png";
 
-var source2=randomimage2;
+var source2="./image/"+randomimage2;
 
 document.querySelector(".img2").setAttribute("src",source2);
 
